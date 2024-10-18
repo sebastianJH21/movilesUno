ef3d5eb74fbaf3ec2ec108ff035fb7bc4180ccbe
lina marin <linamarin2011@gmail.com>
2024-10-18 18:57:53 -0500

    Explicacion de poliformismo en la POO

---
+++ src/main/java/com/example/Vehiculo.java
@@ -0,0 +1,24 @@
+package com.example;
+
+public class Vehiculo {
+
+    public void acelerate (){
+        System.out.println("El vehiculo esta acelerando");
+    }
+
+}
+
+class Auto extends  Vehiculo {
+    public void acelerate (){
+        System.out.println("El vehiculo esta acelerando a 100 Km/h");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Vehiculo myVehiculo1 = new Vehiculo();
+        myVehiculo1.acelerate();
+        Vehiculo myVehiculo2 = new Auto();
+        myVehiculo2.acelerate();
+    }
+}
